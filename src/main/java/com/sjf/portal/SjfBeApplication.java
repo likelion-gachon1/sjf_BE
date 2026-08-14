aaa08217a527497da4246b8cce07ce6a743e64ef
milin0808 <m2njoung@gachon.ac.kr>
2026-08-14 21:56:40 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/sjf/portal/SjfBeApplication.java
@@ -0,0 +1,13 @@
+package com.sjf.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjfBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjfBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjf/portal/SjfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjf.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjfBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
